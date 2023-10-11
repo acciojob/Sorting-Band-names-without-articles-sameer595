5d1e75b06154e2b235abd53effe1514212e2afca
sameer595 <142083277+sameer595@users.noreply.github.com>
2023-10-11 13:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
+function hasKey(key) {
+  // Check if the key exists in the sampleObject
+  return sampleObject.hasOwnProperty(key);
+}
+
+// Do not change the code below
+const key = prompt("Enter Key.");
+alert(hasKey(key));
